ef91f71d3ad8f86656077a6695f93773f3a4304c
Tim Jacomb <timjacomb1@gmail.com>
2025-07-01 14:44:27 +0100

    Debug local-exec failing

--- modules/terraform-zscc-function-app-azure/versions.tf
+++ modules/terraform-zscc-function-app-azure/versions.tf
@@ -8,6 +8,10 @@ terraform {
       source  = "hashicorp/local"
       version = "~> 2.5.0"
     }
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3"
+    }
   }
   required_version = ">= 0.13.7, < 2.0.0"
 }
